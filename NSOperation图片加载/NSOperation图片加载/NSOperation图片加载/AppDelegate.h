ebf2dc92d18a412c4e409f28bd75a4dce261bd3e
wangzhen <869158130@qq.com>
2018-01-02 16:55:51 +0800

    NSOperation图片加载

---
+++ NSOperation图片加载/NSOperation图片加载/NSOperation图片加载/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSOperation图片加载
+//
+//  Created by wangzhen on 2018/1/2.
+//  Copyright © 2018年 wz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
